132352f039c694cb1d88c3cb0b36d6d22b48ba7e
apollo3744-cloud <apollo3744@gmail.com>
2026-09-23 21:04:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='xrp-tick-pwa-v1',A=['./','./index.html','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
