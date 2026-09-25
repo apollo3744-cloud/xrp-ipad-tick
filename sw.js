ec736fa8deefeadc953abdb4cf276faa0c0300d8
apollo3744-cloud <apollo3744@gmail.com>
2026-09-25 21:13:47 +0900

    Update sw.js to v4.3 R11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='xrp-tick-pwa-v4-2-resumable',A=['./','./index.html','./app.js','./manifest.json','./icon.svg'];
+const C='xrp-tick-pwa-v4-3-r11',A=['./','./index.html','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
