ab221e8cd4dab73f19a3a94d97fdf61bc011c81a
apollo3744-cloud <apollo3744@gmail.com>
2026-09-27 02:33:58 +0900

    Refresh cache for zoom update

--- sw.js
+++ sw.js
@@ -1,4 +1,42 @@
-const C='xrp-tick-pwa-v4-3-r11-idb1';,A=['./','./index.html','./app.js','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C='xrp-tick-pwa-v4-3-r11-idb2-zoom';
+const A=[
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener(
+  'install',
+  e=>e.waitUntil(
+    caches
+      .open(C)
+      .then(c=>c.addAll(A))
+      .then(()=>self.skipWaiting())
+  )
+);
+
+self.addEventListener(
+  'activate',
+  e=>e.waitUntil(
+    caches
+      .keys()
+      .then(keys=>
+        Promise.all(
+          keys
+            .filter(x=>x!==C)
+            .map(x=>caches.delete(x))
+        )
+      )
+      .then(()=>self.clients.claim())
+  )
+);
+
+self.addEventListener(
+  'fetch',
+  e=>e.respondWith(
+    fetch(e.request)
+      .catch(()=>caches.match(e.request))
+  )
+);
